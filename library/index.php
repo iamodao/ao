a1f77af7bcc1b1f666de92a60841934b9f20c7ca
Anthony O. Osawere <anthony@osawere.com>
2020-08-27 07:33:47 +0100

    Update index.php

--- library/index.php
+++ library/index.php
@@ -1,5 +1,23 @@
 <?php
 require 'php/dump.inc';
 require 'php/exit.inc';
+require 'php/file.inc';
+oFile::Inc('php/json.inc');
+oFile::Inc('php/database.inc');
 
+
+
+$dba = array(
+	'host' => "localhost",
+	'user' => "zirc",
+	'password' => "ZirC",
+	'name' => "wowcatholic",
+);
+
+
+$omysqli = new oDatabase($dba);
+$ao['MySQLi'] = $omysqli->connect();
+$opdo = new oDatabase($dba, 'pdo');
+$ao['PDO'] = $opdo->connect();
+echo oDump::Neat($ao);
 ?>
