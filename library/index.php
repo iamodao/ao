0c4b0a6922f4502331568173eb463fa522b9dc84
Anthony O. Osawere <anthony@osawere.com>
2020-08-27 12:17:58 +0100

    Update index.php

--- library/index.php
+++ library/index.php
@@ -1,14 +1,21 @@
 <?php
 require 'config.inc';
+
 require 'php/dump.inc';
 require 'php/exit.inc';
 require 'php/file.inc';
 oFile::Inc('php/json.inc');
 oFile::Inc('php/database.inc');
 
-$omysqli = new oDatabase($dba);
-$ao['MySQLi'] = $omysqli->connect();
-$opdo = new oDatabase($dba, 'pdo');
-$ao['PDO'] = $opdo->connect();
-echo oDump::Neat($ao);
+$database = new oDatabase($dba, 'pdo');
+$dbo = $database->connect();
+
+$stmt = $dbo->prepare("CALL DrinkAll()");
+$exec = $stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if($exec != false && !empty($result)){
+	oJSON::run($result, 'oPRINT');
+}
+
+echo oDump::Neat($result);
 ?>
