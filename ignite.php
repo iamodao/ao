89d04fe40c25983acc39a56a851e8abc6b7f4aa2
Anthony O. Osawere <anthony@osawere.com>
2020-09-03 17:24:33 +0100

    HARVEST

--- ignite.php
+++ ignite.php
@@ -1,38 +1,44 @@
 <?php
 /**ORAX™ Framework is a vanilla and evolving framework for developing websites, applications, and APIs using web technology.
-* Creator: Osawere™ O. Anthony - @iamodao - www.osawere.com  © August 2020 | Apache License
-* ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ===  ==
-* INDEX ~ Default File • DEPENDENCY»
-*/
+ * Creator: Osawere™ O. Anthony - @iamodao - www.osawere.com  © August 2020 | Apache License
+ * ==== === ===== === ===== === ===== ======= ==== ========= ==== ==== ======= ======== ======== === ===
+ * DEFAULT ~ Index Handler • DEPENDENCY»
+ */
 
-#DEFINE SEPARATOR & DIRECTORY
+#DEFINE SEPARATOR
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 defined('PS') ? null : define('PS', '/');
+
+#DEFINE DIRECTORY
 defined('ROOT') ? null : define('ROOT', __DIR__.DS);
 defined('ORAX') ? null : define('ORAX', ROOT.'orax'.DS);
-defined('ESSENTIAL') ? null : define('ESSENTIAL', ORAX.'essential'.DS);
+defined('oPHPD') ? null : define('oPHPD', ORAX.'ophp'.DS);
 defined('SOURCE') ? null : define('SOURCE', ROOT.'source'.DS);
 
-// #REQUIRED ~ ESSENTIAL LIBRARY
-// if(!file_exists(ESSENTIAL.'reporting.inc')){
-// 	exit('<strong>oESSENTIAL::</strong> Resource Unavailable [<em>'.ESSENTIAL.'reporting.inc'.'</em>]');
-// } else {
-// 	require ESSENTIAL.'reporting.inc';
-// 	if(!file_exists(ESSENTIAL.'file.inc')){
-// 		o404EXIT(ESSENTIAL.'file.inc');
-// 	}
-// 	require ESSENTIAL.'file.inc';
-// 	oFile::isCheck(ORAX.'library.inc');
-// 	require ORAX.'library.inc';
-// 	oFile::isCheck(SOURCE.'config.inc');
-// 	require SOURCE.'config.inc';
-// 	oFile::isCheck(SOURCE.'init.inc');
-// 	require SOURCE.'init.inc';
+#REQUIRE ESSENTIAL
+require oPHPD.'dump.inc';
+require oPHPD.'exit.inc';
+if(!file_exists(oPHPD.'file.inc')){oExit::NotFound(oPHPD.'file.inc');}
+require oPHPD.'file.inc';
+require oPHPD.'router.inc';
 
-// 	#SANDBOX FILE - for development, demo & testing
-// 	$o_debug_file = '_ignore'.DS.'debug.inc';
-// 	if(oFile::is($o_debug_file)){
-// 		require $o_debug_file;
-// 	}
-// }
+#INCLUDE OTHER [project specific] LIBRARY
+// oFile::Inc(SOURCE.'config.inc');
+
+$o_source = oRouter::Source();
+$o_link = oRouter::Link();
+$o_route = oRouter::Route();
+
+
+#HARVERST APP
+if($o_source == 'harvest'){
+	echo $oHarvest['RD'] = SOURCE.$o_source.DS;
+	if(oFile::Is($oHarvest['RD'].$o_link.'.inc')){require ($oHarvest['RD'].$o_link.'.inc');}
+
+}
+
+
+#SANDBOX FILE - for development, demo & testing
+$o_debug_file = ROOT.'_ignore'.DS.'debug.inc';
+if(oFile::is($o_debug_file)){require $o_debug_file;}
 ?>
