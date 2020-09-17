3ddd3154e02b3a1b0bf20a41cfe79378a0942acd
Anthony O. Osawere <anthony@osawere.com>
2020-09-17 12:27:13 +0100

    DONEISH

--- ignite_.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'orax/ophp/libry/class/dump.inc';
-require 'orax/ophp/libry/class/string.inc';
-
-
-$string = 'John_K__K_Doe';
-echo oDump::Neat(oString::To($string, '_K_', ' K.' , 'oLAST', $case_sensitive=false));
-?>
